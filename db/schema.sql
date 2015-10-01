0075e32ba9318449500c814c69bada45e8f820bc
joey.rivera <joey.rivera@salesforce.com>
2015-10-01 13:47:00 -0400

    adding db schema diagram

--- db/schema.sql
+++ db/schema.sql
@@ -5,32 +5,29 @@ SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='TRADITIONAL,ALLOW_INVALID_DATES';
 
 -- -----------------------------------------------------
--- Schema exam_prep
+-- Schema app
 -- -----------------------------------------------------
 DROP SCHEMA IF EXISTS `app` ;
 
 -- -----------------------------------------------------
--- Schema exam_prep
+-- Schema app
 -- -----------------------------------------------------
-CREATE SCHEMA IF NOT EXISTS `app` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci ;
-SHOW WARNINGS;
+CREATE SCHEMA IF NOT EXISTS `app` DEFAULT CHARACTER SET utf8 ;
 USE `app` ;
 
 -- -----------------------------------------------------
--- Table `exam_prep`.`user`
+-- Table `app`.`user`
 -- -----------------------------------------------------
-DROP TABLE IF EXISTS `app`.`user` ;
-
-SHOW WARNINGS;
 CREATE TABLE IF NOT EXISTS `app`.`user` (
-  `id` INT NOT NULL AUTO_INCREMENT,
+  `id` INT(11) NOT NULL AUTO_INCREMENT,
   `first_name` VARCHAR(100) NOT NULL,
   `last_name` VARCHAR(100) NOT NULL,
   `created` TIMESTAMP NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`))
-ENGINE = InnoDB;
+ENGINE = InnoDB
+AUTO_INCREMENT = 1
+DEFAULT CHARACTER SET = utf8;
 
-SHOW WARNINGS;
 
 SET SQL_MODE=@OLD_SQL_MODE;
 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
